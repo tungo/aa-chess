0b7519b96d504ea022e003ff1754535bd3508c64
Tu Ngo <tungo8810@gmail.com>
2017-04-03 13:48:42 -0700

    Add move_piece method

--- chess/board.rb
+++ chess/board.rb
@@ -3,17 +3,34 @@ require_relative 'piece'
 class Board
   def initialize()
     @grid = []
-    2.times { @grid << setup_row_pieces }
-    4.times { @grid << setup_row_null_pieces }
-    2.times { @grid << setup_row_pieces }
+    setup_grid
   end
 
-  def setup_row_null_pieces()
-    Array.new(8) { NullPiece.new }
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
   end
 
-  def setup_row_pieces()
-    Array.new(8) { Piece.new }
+  def []=(pos, val)
+    row, col = pos
+    @grid[row][col] = val
+  end
+
+  def move_piece(start_pos, end_pos)
+    if self[start_pos].is_a?(NullPiece)
+      raise("Wrong start position, no piece at this position.")
+    elsif self[end_pos].is_a?(Piece)
+      raise("Wrong end position, there is a piece at this position")
+    end
+
+    self[end_pos] = self[start_pos]
+    self[start_pos] = NullPiece.new
+  end
+
+  def setup_grid
+    2.times { @grid << Array.new(8) { Piece.new } }
+    4.times { @grid << Array.new(8) { NullPiece.new } }
+    2.times { @grid << Array.new(8) { Piece.new } }
   end
 
 end
